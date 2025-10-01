cc6fc7e5d5f347bc7084acb377164a5e1adb12ee
Julio Godoy <juliogodoy1012@gmail.com>
2025-10-01 12:02:30 -0600

    actualizando servidor

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-import express from "express"
-import mysql from "mysql2"
+import express from "express";
+import mysql from "mysql2";
+import path from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 
 const app = express()
 const port = 3500
 
 app.use(express.json())
+app.use(express.static(path.join('public')));
+
 
 const conexion = mysql.createConnection(
     {
@@ -26,10 +32,10 @@ app.get("/server", (req, res) => {
     res.send("Servidor funcionando correctamente")
 })
 
-app.get("/", (req, res))=>{
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 }
-
+)
 
 app.get("/usuarios", (req, res) => {
     const query = "SELECT * FROM usuarios"
